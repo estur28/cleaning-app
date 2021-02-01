2d31dbe00e23045c6bb188537852e3118173acda
estur28 <89254564126@mail.ru>
2021-02-01 18:29:56 +0300

    feat: added modal logic

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,29 @@
-$(function () {
+const toggleModal = () => {
+    document.querySelector('.modal')
+        .classList.toggle('modal--hidden');
+    document.querySelector('.overlay')
+        .classList.toggle('overlay--hidden');
+}
+
+document.querySelector('#modal-btn')
+    .addEventListener('click', toggleModal);
+
+document.querySelector('.overlay')
+    .addEventListener('click', toggleModal)
+
+document.querySelector('#learn-more-form')
+    .addEventListener('submit', (e) => {
+        e.preventDefault();
+        toggleModal();
+})
+
+document.querySelector('.close')
+    .addEventListener('click', toggleModal);
+
+document.querySelector('.overlay')
+    .addEventListener('click', toggleModal);
 
 
- $('body').css('background-color', 'blue');
-
-
-});
+function sendFields() {
+    document.getElementById('input-email').value = '';
+}
